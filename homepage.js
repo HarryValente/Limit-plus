9cc826174698b2acbd0c0fc6031960fdbec6d371
HarryValente <harry-joevalente@live.co.uk>
2020-12-07 00:27:58 +0000

    Search button added
    Later down the line it will need functioning with a fetch API

--- homepage.js
+++ homepage.js
@@ -1,5 +1,18 @@
+// Assaigning variable for header sticky change
 let header = document.querySelector('header')
 
+// If scrolling down is greater than 0 class of sticky is assaigned to header
 window.addEventListener('scroll',  function() {
     header.classList.toggle('sticky', window.scrollY > 0)
+})
+
+// Assigning variables for hidden search bar
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.input')
+
+// When the btn is clicked .active is added
+btn.addEventListener('click', () => {
+    search.classList.toggle('active')
+    input.focus()
 })
