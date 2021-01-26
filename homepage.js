d1725460a9b7afc0bf5ad6fe40215e1ae1ccb374
HarryValente <harry-joevalente@live.co.uk>
2021-01-26 17:21:55 +0000

    Adding of a feature swiper
    Also sorted out the search bar and put it in a more reasonable location

--- homepage.js
+++ homepage.js
@@ -118,3 +118,21 @@ searchBar.addEventListener('submit', (e) => {
         window.location.reload()
     }
 })
+
+var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
