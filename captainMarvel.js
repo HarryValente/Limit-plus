bcdc16c33ff7e359a841d8073f8233e794866fce
HarryValente <harry-joevalente@live.co.uk>
2021-02-25 18:22:57 +0000

    Swiper is implemented and beginnings of responsiveness is there

--- captainMarvel.js
+++ captainMarvel.js
@@ -5,3 +5,37 @@ let header = document.querySelector('header')
 window.addEventListener('scroll',  function() {
     header.classList.toggle('sticky', window.scrollY > 0)
 })
+
+// Related underline decoration
+let marker = document.getElementById('marker')
+let item = document.querySelectorAll('.relatedList')
+
+// A function which changes the left and width of the marker
+function indicator(e) {
+    marker.style.left = e.offsetLeft+"px"
+    marker.style.width = e.offsetWidth+"px"
+}
+
+// When a link is clicked it runs the function above and fills in the "px's"
+item.forEach(link => {
+    link.addEventListener('click', (e) => {
+        indicator(e.target)
+    })
+})
+
+// Swiper.JS
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
